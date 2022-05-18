e23d2056d421e9eeed420c1332eb419851baa730
ABDELKAWY A. ABDELAZIZ <abdelkawy.abdelhady@gmail.com>
2022-05-18 22:30:18 +0200

    Fixed deploy issue

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   appName: 'abdelkawy-udagram-api',
   apiHost: 'http://abdelkawy-udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0'
   //apiHost: 'http://localhost:3000/api/v0'
