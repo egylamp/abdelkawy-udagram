5261bd00aace83e981330d4a66aeb46052442ea8
ABDELKAWY A. ABDELAZIZ <abdelkawy.abdelhady@gmail.com>
2022-05-18 23:44:25 +0200

    all

--- udagram-frontend/src/environments/environment.prod.ts
+++ udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: true,
   appName: "abdelkawy-dagram-api",
-  apiHost: 'http://abdelkawy-udagram-api-dev.us-east-1.elasticbeanstalk.com:3000/api/v0',
+  apiHost: 'http://abdelkawy-udagram-api-dev.us-east-1.elasticbeanstalk.com/api/v0',
   //apiHost: "http://localhost:8080/api/v0",
 };
 
